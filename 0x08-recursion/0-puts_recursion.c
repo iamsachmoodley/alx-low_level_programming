82dbde90e84c9c8d24c6b50e686362650e0c0aa7
iamsachmoodley <sachmoodley@gmail.com>
2023-03-08 09:04:02 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- *  _puts_recursion - Prints a string in reverse.
+ *  _puts_recursion - Prints a string in recursivly
  * @s: The string to be printed.
  */
 void  _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == 0)
 	{
-	        _puts_recursion(s + 1);
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
+
+_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
