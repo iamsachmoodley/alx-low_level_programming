9b6cc834e17ab5e29f1fc7229fc29eff5ffb0820
iamsachmoodley <sachmoodley@gmail.com>
2023-05-23 09:01:58 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - print_alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 1)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
